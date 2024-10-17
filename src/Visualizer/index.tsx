1b33c4d49cbbc1ddd5b9e6adbe74d46d0b62cac0
Antonia Elsen <antoniaelsen@gmail.com>
2024-10-17 02:01:30 -0700

    fix: fix issue where app would crash on switching visualizers

--- src/Visualizer/index.tsx
+++ src/Visualizer/index.tsx
@@ -2,6 +2,8 @@ import { Box, BoxProps } from "@mui/material";
 import { ErrorBoundary, ErrorText } from "components/ErrorBoundary";
 import { useAppStore } from "../store/app";
 import { getVisualizer } from "./visualizers";
+import { Suspense } from "react";
+import { LoadingSpinner } from "components/LoadingSpinner";
 
 export const Visualizer = (props: BoxProps) => {
   const visualizer = useAppStore((state) => state.visualizer);
@@ -20,7 +22,19 @@ export const Visualizer = (props: BoxProps) => {
           <ErrorText title="oh no" subtitle="does your device support webgl?" />
         }
       >
-        <Scene />
+        <Suspense
+          fallback={
+            <LoadingSpinner
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+              }}
+            />
+          }
+        >
+          <Scene />
+        </Suspense>
       </ErrorBoundary>
     </Box>
   );
